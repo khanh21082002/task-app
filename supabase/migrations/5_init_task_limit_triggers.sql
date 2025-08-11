fc557596fa3d064260393ef35b36f65fdabe2fae
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-08-11 08:44:15 +0700

    modify: add 10 point bonus to get_score method

--- supabase/migrations/5_init_task_limit_triggers.sql
+++ supabase/migrations/5_init_task_limit_triggers.sql
@@ -1,1 +1,10 @@
+class User:
+    def __init__(self, age, score):
+        self.age = age
+        self.score = score
 
+    def get_age(self):
+        return self.age
+
+    def get_score(self):
+        return self.score + 10
