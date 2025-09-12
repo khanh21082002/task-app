4755db1e68263e686ea119863b8123d9229ec271
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-09-12 04:45:44 +0000

    refactor: adjust table header column widths and padding

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -3,36 +3,37 @@ import {
   TableBody,
   TableHead,
   TableHeader,
+  TableHeaderCell,
   TableRow,
 } from "@/components/ui/table";
 import TaskRow from "./TaskRow";
 import { Task } from "@/types/models";
 
 interface TaskListProps {
-  tasks: Task[];
-  onDelete: (taskId: string) => Promise<void>;
-  onToggleComplete: (taskId: string, completed: boolean) => Promise<void>;
+  taskList: Task[];
+  onTaskDelete: (taskId: string) => Promise<void>;
+  onTaskCompleteToggle: (taskId: string, completed: boolean) => Promise<void>;
 }
 
-const TaskList = ({ tasks, onDelete, onToggleComplete }: TaskListProps) => {
+const TaskList = ({ taskList, onTaskDelete, onTaskCompleteToggle }: TaskListProps) => {
   return (
     <Table>
       <TableHeader>
         <TableRow className="hover:bg-transparent">
-          <TableHead className="w-[50px] py-2"></TableHead>
-          <TableHead className="py-2">Title</TableHead>
-          <TableHead className="w-[100px] py-2">Label</TableHead>
-          <TableHead className="w-[120px] py-2">Due Date</TableHead>
-          <TableHead className="w-[100px] text-right py-2">Actions</TableHead>
+          <TableHeaderCell className="w-[50px] py-2"></TableHeaderCell>
+          <TableHeaderCell className="py-2">Title</TableHeaderCell>
+          <TableHeaderCell className="w-[100px] py-2">Label</TableHeaderCell>
+          <TableHeaderCell className="w-[120px] py-2">Due Date</TableHeaderCell>
+          <TableHeaderCell className="w-[100px] text-right py-2">Actions</TableHeaderCell>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {tasks.map((task) => (
+        {taskList.map((currentTask) => (
           <TaskRow
-            key={task.task_id}
-            task={task}
-            onDelete={onDelete}
-            onToggleComplete={onToggleComplete}
+            key={currentTask.task_id}
+            task={currentTask}
+            onDelete={onTaskDelete}
+            onToggleComplete={onTaskCompleteToggle}
           />
         ))}
       </TableBody>
