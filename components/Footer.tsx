9fda6499706af94186181b21e322885a91891716
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-09-12 04:34:33 +0000

    refactor: simplify footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
-const Footer = () => (
-  <footer className="bg-transparent text-white py-4" />
-);
+const FooterComponent = () => {
+  return (
+    <footer className="bg-transparent text-white py-4" />
+  );
+};
 
-export default Footer;
+export default FooterComponent;
