e7a51fb69b41a450440f94fa72b1b47844f0ddb4
khanh21082002 <nguyenkhanhduyenhai@gmail.com>
2025-09-12 05:10:53 +0000

    modify: adjust score calculation in User class

--- supabase/migrations/5_init_task_limit_triggers.sql
+++ supabase/migrations/5_init_task_limit_triggers.sql
@@ -1,10 +1,11 @@
 class User:
-    def __init__(self, age, score):
+    def __init__(self, age: int, score: int):
         self.age = age
         self.score = score
+        self.adjusted_score = score + 10
 
-    def get_age(self):
+    def get_age(self) -> int:
         return self.age
 
-    def get_score(self):
-        return self.score + 10
+    def get_score(self) -> int:
+        return self.adjusted_score
